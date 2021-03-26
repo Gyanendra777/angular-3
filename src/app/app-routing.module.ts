8a316fd229c5f124cb35ef3b641f9310e8531e66
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-03-26 20:20:52 +0530

    angular routing in page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContantComponent } from './contant/contant.component';
+import { ProductComponent } from './product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: 'HomeComponent', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'product', component: ContantComponent },
+  { path: 'contant', component: ProductComponent },
+  // { path: '',   redirectTo: '/', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
