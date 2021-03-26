17543b943166bcf81f3f9c01fe0c1fd6db9d3553
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-03-26 21:24:07 +0530

    routing  redayerekt and routint nhi milne pr not fout pr chla jata h

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,17 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContantComponent } from './contant/contant.component';
 import { ProductComponent } from './product/product.component';
+import { PagenotfountComponent } from './pagenotfount/pagenotfount.component';
 
 const routes: Routes = [
-  { path: '',   redirectTo: 'HomeComponent', pathMatch: 'full' },
-  { path: '', component: HomeComponent },
+  { path: '',   redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'product', component: ContantComponent },
   { path: 'contant', component: ProductComponent },
-  // { path: '',   redirectTo: '/', pathMatch: 'full' },
+  { path: '**', component: PagenotfountComponent },
+
+ 
 ];
 
 @NgModule({
