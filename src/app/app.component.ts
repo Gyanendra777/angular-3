747f521c66f9ed73570c47432f899ee85a3f58a5
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-03-26 15:10:00 +0530

    ngSwitch-directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'text4';
-  property:boolean =true;
 
-  goer(){
-    this.property=false;
+  Selectproduct:string;
+  
+  getvalu(drapd){
+    console.log(drapd.target.value);
+    this.Selectproduct=drapd.target.value;
   }
+
 }
