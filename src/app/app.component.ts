a0f7e0e69ca0a25778d378eb212a4f4dcdbd53cf
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-03-26 16:47:32 +0530

    Push and Splice with ngFor in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'text4';
+  uuname
+  user=[]
+  getvalu(uname){
+      this.user.push({
+        name:uname.value
+      })
+      this.uuname=""
 
+  }
+  getvalureplace(){
+    this.user.splice(this.user.length -1)
+  }
+  onRemoveuser(i){
+    this.user.splice(i, 1)
+  }
 }
