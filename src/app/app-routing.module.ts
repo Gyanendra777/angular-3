aaee60d57dd84d94420ebe0938da796f85a1206a
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-03-27 09:55:10 +0530

    Angular 10|9|8 Nested Routing with Multiple RouterOutlet using loadChildren having own Router Modules Example Application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AttendanceComponent } from './attendance/attendance.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Page404Component } from './page404/page404.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'attendance', component: AttendanceComponent },
+  { path: 'leaves', loadChildren: () => import(`./leaves/leaves.module`).then(m => m.LeavesModule) },
+  // { path: 'leaves', loadChildren: './leaves/leaves.module#LeavesModule' },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', component: Page404Component },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [ RouterModule.forRoot(routes,{ enableTracing: false })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
