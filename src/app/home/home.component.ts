aa931b3f8a672267e05ea2ce03094e3a54fa846b
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-03-30 11:40:37 +0530

    radobutton in dayanemek button and data view se component me send kya useke bd component se view me send kya gya h user ke pss

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,13 +9,57 @@ import { NgForm } from '@angular/forms';
 export class HomeComponent implements OnInit {
 @ViewChild('simpleform') myform:NgForm
 
+genders = [
+  {id:"1", gender:'male'},
+  {id:"2", gender:'female'}
+];
+defoultvaluee="female";
 usernamee="gyanendra dubey"
 
-fistname
+setva(){
+  this.myform.setValue({
+    
+    userditel:{
+      fast:"RAJU",
+      last:"AAAAAAA"
+    },
+    address:"",
+    email:"",
+    gender:""
+
+})
+}
+pachva(){
+  this.myform.form.patchValue({
+    
+    userditel:{
+      fast:"raju",
+      last:"dubey"
+    },
+})
+}
+uaered={
+  fast:'',
+  last:'',
+  address:'',
+  email:'',
+  gender:''
+}
+
+
   valusend(val){
-    this.fistname = val.userditel.fast
-      console.log(this.fistname)
-      console.log(val)
+   this.uaered.fast = val.userditel.fast;
+   this.uaered.last = val.userditel.last;
+   this.uaered.address = val.address;
+   this.uaered.email = val.email;
+   this.uaered.gender = val.gender;
+      console.log(this.uaered.fast);
+      console.log(this.uaered.last);
+      console.log(this.uaered.address);
+      console.log(this.uaered.email);
+      console.log(this.uaered.gender);
+      console.log(val);
+      this.myform.reset();
   }
   constructor( ) { }
 
