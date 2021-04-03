df219b7e4ce874d55383a5d3c06a1f113c6dad23
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-04-03 17:29:25 +0530

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
-import { OutputingComponent } from './outputing/outputing.component';
-import { ChildComponent } from './child/child.component';
-
-
+import { FilterPipe } from './filter.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
-    OutputingComponent,
-    ChildComponent,
- 
-   
+    FilterPipe,
    
   ],
   imports: [
