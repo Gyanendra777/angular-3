90d6553536bf3a056569b787f38562c759a422d9
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-04-05 07:23:48 +0530

    khalli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
-import { FilterPipe } from './filter.pipe';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
-    FilterPipe,
+   
    
   ],
   imports: [
